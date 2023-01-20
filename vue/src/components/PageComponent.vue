3604e425d0c9991d017518dd81aff36e62555c6c
MASD981022 <dasalvasuper@gmail.com>
2023-01-20 13:47:33 -0600

    ProjectView component

--- vue/src/components/PageComponent.vue
+++ vue/src/components/PageComponent.vue
@@ -6,9 +6,9 @@
       </div>
     </header>
     <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 bg-gray-100">
         <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
+        <div class="px-4 py-6 sm:px-0 ">
             <slot name="header"></slot>
         </div>
         <!-- /End replace -->
--- vue/src/views/ProjectView.vue
+++ vue/src/views/ProjectView.vue
@@ -1,13 +1,144 @@
 <template>
-  <PageComponent title="View Or Create new Project">
+  <PageComponent :title=" model.id ? model.title : 'Create Project'">
+      <template v-slot:header>
     
+      <form @submit.prevent="saveSurvey">
+        <div class="shadow sm:rounded-md sm:overflow-hidden">
+          <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+
+        <!-- Image -->
+          <div>
+            <label class="block text-sm font-medium text-gray-700">
+              Image
+            </label>
+            <div class="mt-1 flex items-center">
+              <img class="w-64 h-48 object-cover" v-if="model.image" :src="model.image" alt="model.title">
+              <span v-else class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100" >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  </svg>
+   
+
+              </span>
+             <button
+                type="button"
+                class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <input
+                  type="file"
+                  @change="onImageChoose"
+                  class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"
+                />
+                Change
+              </button>
+            </div>
+          </div>
+          <!-- Image -->
+
+      <!-- Title -->
+          <div>
+            <label for="title" class="block text-sm font-medium text-gray-700"
+              >Title</label
+            >
+            <input
+              type="text"
+              name="title"
+              id="title"
+              v-model="model.title"
+              autocomplete="project_slug"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <!--/ Title -->
+
+       <!-- Slug -->
+          <div>
+            <label for="title" class="block text-sm font-medium text-gray-700"
+              >Slug</label
+            >
+            <input
+              type="text"
+              name="slug"
+              id="slug"
+              v-model="model.slug"
+              autocomplete="project_slug"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <!--/ Slug -->
+          
+         <!-- Description -->
+          <div>
+            <label for="about" class="block text-sm font-medium text-gray-700">
+              Description
+            </label>
+            <div class="mt-1">
+              <textarea
+                id="description"
+                name="description"
+                rows="3"
+                v-model="model.description"
+                autocomplete="project_description"
+                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                placeholder="Describe your project"
+              />
+            </div>
+          </div>
+          <!-- Description -->
+
+         <!-- responsable -->
+          <div>
+            <label for="about" class="block text-sm font-medium text-gray-700">
+              Responsible
+            </label>
+            <div class="mt-1">
+              <select name="responsible" id="responsible" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+>
+                <option value="David">David</option>
+              </select>
+            </div>
+          </div>
+          <!-- Description -->
+
+          </div>
+          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white
+          bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          
+            Save
+          </button>
+        </div>
+        </div>
+      </form>
+       </template>
   </PageComponent>
 </template>
 
-<script>
-export default {
+<script setup>
+import store from "../store"
+import {ref} from "vue"
+import PageComponent from "../components/PageComponent.vue"
+import {useRoute} from "vue-router"
+
+const route=useRoute()
+
+
+//Create empty project
+let model= ref({
+  title:"",
+  slug:"",
+  image:null,
+  description:null,
+  user_id:null
+})
+
+if(route.params.id){
+  model.value = store.state.projects.find(
+    (p) => p.id === parseInt(route.params.id)
+  )
 
 }
+
 </script>
 
 <style>
